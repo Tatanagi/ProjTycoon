1d405b957ca7a7af4d23374c1f91144134f9f013
Tatanagi <kayrafaelchester1925@gmail.com>
2025-06-17 20:04:06 +0800

    Added Dice Movement and 2.0 Prefabs and remove the scene folder

--- Assets/Scipts/Inigo/Game/PlayerController.cs
+++ Assets/Scipts/Inigo/Game/PlayerController.cs
@@ -1,5 +1,4 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
@@ -8,13 +7,18 @@ public class PlayerController : MonoBehaviour
     public float moveSpeed = 2f;
     private int currentCellIndex = 0;
 
+    public bool IsFinishedMoving { get; private set; } = true;
+
     public void MovePlayer(int steps)
     {
-        StartCoroutine(MoveSteps(steps));
+        if (IsFinishedMoving)
+            StartCoroutine(MoveSteps(steps));
     }
 
     private IEnumerator MoveSteps(int steps)
     {
+        IsFinishedMoving = false;
+
         for (int i = 0; i < steps; i++)
         {
             currentCellIndex = (currentCellIndex + 1) % boardCells.Length;
@@ -23,23 +27,97 @@ public class PlayerController : MonoBehaviour
             while (Vector3.Distance(transform.position, targetPos) > 0.01f)
             {
                 transform.position = Vector3.MoveTowards(transform.position, targetPos, moveSpeed * Time.deltaTime);
-
                 yield return null;
             }
 
+            transform.position = targetPos;
             yield return new WaitForSeconds(0.2f);
         }
+
+        string landedTag = boardCells[currentCellIndex].tag;
+        Debug.Log("Landed on: " + landedTag);
+
+        HandleCellAction(landedTag);
+
+        IsFinishedMoving = true;
     }
 
-    // Start is called before the first frame update
-    void Start()
+    private void HandleCellAction(string tag)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        switch (tag)
+        {
+            case "Start":
+                Debug.Log("You are at the start!");
+                break;
+            case "Fish":
+            case "Fish 2":
+                Debug.Log("You caught some fish!");
+                break;
+            case "Fishery":
+                Debug.Log("You arrived at the fishery!");
+                break;
+            case "House":
+            case "House With Grass":
+                Debug.Log("You entered a house.");
+                break;
+            case "Stone Quarry":
+                Debug.Log("You found some stone!");
+                break;
+            case "Wheat Field":
+                Debug.Log("You harvested wheat!");
+                break;
+            case "Wishing Well With Rocks":
+                Debug.Log("You found a wishing well!");
+                break;
+            case "Lucky Loan Lender":
+            case "Loan Lender":
+                Debug.Log("You can get a loan here.");
+                break;
+            case "Silk":
+                Debug.Log("You found valuable silk!");
+                break;
+            case "Bag":
+                Debug.Log("You found a mysterious bag.");
+                break;
+            case "Goats":
+                Debug.Log("You encountered some goats.");
+                break;
+            case "Tree":
+                Debug.Log("You found a tree.");
+                break;
+            case "Royal Mint":
+                Debug.Log("You reached the royal mint. Gain coins!");
+                break;
+            case "Wool":
+                Debug.Log("You collected wool.");
+                break;
+            case "Lettuce":
+                Debug.Log("You harvested lettuce.");
+                break;
+            case "Hat":
+                Debug.Log("You found a stylish hat.");
+                break;
+            case "Tile Branch":
+                Debug.Log("A branch blocks your path.");
+                break;
+            case "Village Fair":
+                Debug.Log("Enjoy the fair!");
+                break;
+            case "Turnips":
+                Debug.Log("You found turnips.");
+                break;
+            case "Stabie":
+                Debug.Log("You met Stabie the NPC.");
+                break;
+            case "Community Chess":
+                Debug.Log("Draw a community card!");
+                break;
+            case "Blank Space":
+                Debug.Log("Just passing through.");
+                break;
+            default:
+                Debug.Log("Landed on an unknown tile: " + tag);
+                break;
+        }
     }
 }
---
+++ Assets/Scipts/Kyle/Dice.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Dice : MonoBehaviour
+{
+    public PlayerController player;
+    public Image diceImage;            // UI Image to show dice face
+    public Sprite[] diceSides;         // Array of dice face sprites (1 to 6)
+    public Button rollButton;          // Button to roll the dice
+
+    private bool coroutineAllowed = true;
+
+    void Start()
+    {
+        // Load dice sprites from Resources/DiceSprites folder
+        diceSides = Resources.LoadAll<Sprite>("DiceSprites");
+
+        if (rollButton != null)
+            rollButton.onClick.AddListener(() => StartCoroutine(RollDice()));
+    }
+
+    private IEnumerator RollDice()
+    {
+        if (!coroutineAllowed || player == null)
+            yield break;
+
+        coroutineAllowed = false;
+
+        int randomDiceNumber = Random.Range(1, 7); // 1–6
+        Debug.Log("Rolled: " + randomDiceNumber);
+
+        if (diceImage != null && diceSides.Length >= 6)
+        {
+            diceImage.sprite = diceSides[randomDiceNumber - 1];
+        }
+
+        player.MovePlayer(randomDiceNumber);
+
+        yield return new WaitUntil(() => player.IsFinishedMoving);
+        coroutineAllowed = true;
+    }
+}
