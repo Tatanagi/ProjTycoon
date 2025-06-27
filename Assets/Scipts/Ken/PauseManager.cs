336712f5b5c3a807322fe69528d5acdaa49acbb8
KenChiimken <xxkenxx02@gmail.com>
2025-06-27 23:57:57 +0800

    Button Functions Applied

---
+++ Assets/Scipts/Ken/PauseManager.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PauseManager : MonoBehaviour
+{
+    [Header("UI References")]
+    [SerializeField] private GameObject pauseCanvas;
+    [SerializeField] private Button pauseButton;
+    [SerializeField] private Button resumeButton;
+    [SerializeField] private Button quitButton;
+
+    private void Start()
+    {
+        // Initialize state
+        pauseCanvas.SetActive(false);
+        Time.timeScale = 1f;
+
+        // Setup button listeners
+        pauseButton.onClick.AddListener(TogglePause);
+        resumeButton.onClick.AddListener(TogglePause);
+        quitButton.onClick.AddListener(() => SceneManager.LoadScene("Main Menu"));
+    }
+
+    public void TogglePause()
+    {
+        bool shouldPause = !pauseCanvas.activeSelf;
+        pauseCanvas.SetActive(shouldPause);
+        Time.timeScale = shouldPause ? 0f : 1f;
+    }
+
+    private void OnDestroy()
+    {
+        Time.timeScale = 1f; // Critical for editor workflow
+    }
+}
